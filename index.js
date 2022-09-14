59fddf4986149dcb0a2f791d8d406f1db0c02f31
thepnatee <thepnatee.p@gmail.com>
2022-09-14 11:19:32 +0700

    1 file modified

--- index.js
+++ index.js
@@ -26,20 +26,35 @@ const friendShip = document.getElementById('friendShip');
 
 async function main() {
   // Initialize LIFF app)
-  await liff.init({ liffId: '1657470837-mD4kzyX0' });
-  // if (!liff.isLoggedIn()) {
-  //   liff.login({ redirectUri: 'https://quickstart-liff-1yjmnv.stackblitz.io' });
-  // }
-  switch (liff.getOS()) {
-    case 'android':
-      body.style.backgroundColor = '#d1f5d3';
-      break;
-    case 'ios':
-      body.style.backgroundColor = '#eeeeee';
-      break;
-  }
+  await liff.init({ liffId: '1656014303-pLEPbw2L' });
+  liff.ready.then(() => {
+    switch (liff.getOS()) {
+      case 'android':
+        body.style.backgroundColor = '#d1f5d3';
+        break;
+      case 'ios':
+        body.style.backgroundColor = '#eeeeee';
+        break;
+    }
+    // do something you want when liff.init finishes
+  });
 
-  getUserProfile();
+  // await liff
+  //   .init({
+  //     liffId: "1656014303-pLEPbw2L'", // Use own liffId
+  //   })
+  //   .then(() => {
+  //     console.log('success');
+  //     alert('test');
+  //     // Start to use liff's api
+  //   })
+  //   .catch((err) => {
+  //     alert(err);
+  //     // Error happens during initialization
+  //     console.log(err.code, err.message);
+  //   });
+
+  // getUserProfile();
   // Try a LIFF function
 }
 
